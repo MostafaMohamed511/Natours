3826327bf94a939541d70db0ef7120e90eaf3d8b
MostafaMohamed511 <mostafa.fcai.gmail.com>
2024-06-19 04:27:21 +0300

    completed security part

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/AppError');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
-app.use(express.json());
-app.use(morgan('dev'));
+// MIDDLEWARES
+
+// set security HTTP headers
+app.use(helmet());
+
+// Limit amout of requests from the same api
+//app.use('/api' , limiter);  // you can use like this to use the middleware with any route starts with /api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this Api. Please try again in an hour',
+});
+app.use(limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'price',
+      'difficulty',
+      'maxGroupSize',
+      'ratingsQuantity',
+      'ratingsAverage',
+    ],
+  })
+);
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,10 +14,15 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  //jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-  // expiresIn: process.env.JWT_EXPIRES,
-  //});
-
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  user.password = undefined;
+  res.cookie('jwt', token, cookieOptions);
   res.status(statusCode).json({
     status: 'success',
     token,
